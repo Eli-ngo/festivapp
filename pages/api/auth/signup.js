7e563bb7882ded2e604c5fac1eb1e3c6c7374d69
Elisabeth Ngo <elisab.ngo@gmail.com>
2022-07-24 23:13:04 +0200

    GTA / GA / flash messages / sign up page

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client'; // by importing the client, we can use the client to make queries to the database
+import { hash } from 'bcryptjs'; // hash le mot de passe en BDD
+
+export default async function signup(req, res) {
+    const prisma = new PrismaClient(); // on crée un nouveau client prisma
+
+    // seul POST est autorisé
+    if(req.method === 'POST'){
+        const { username, lastname, firstname, email, password } = req.body; // on récupère les données du formulaire
+        // on vérifie que les données sont valides
+        if(!username || !email || !email.includes('@') || !password || !lastname || !firstname){
+            res.status(422).json({ error: 'Données invalides' })
+            return
+        }
+
+        // on vérifie que l'utilisateur n'existe pas déjà
+        const checkExisting = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        // si l'utilisateur existe déjà, on renvoie une erreur
+        if(checkExisting){
+            res.status(500).json({ message: 'Utilisateur déjà existant' })
+            return
+        }
+        //si toutes les données sont valides, on crée un nouvel utilisateur dans la BDD
+        const createUser = await prisma.user.create({
+            data: {
+                username,
+                lastname,
+                firstname,
+                email,
+                password: await hash(password, 12),
+            }
+        })
+
+        await prisma.$disconnect()
+        // on renvoie un message de succès
+        res.status(201).json({ message: 'Utilisateur créé' })
+    } else {
+        res.status(500).json({ message: 'Chemin invalide' })
+    }
+}
