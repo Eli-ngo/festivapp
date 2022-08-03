e93cb11ec48403b028d41da61a712a88ad4a948a
Elisabeth Ngo <elisab.ngo@gmail.com>
2022-08-04 00:49:20 +0200

    assigned user to individual post

--- pages/api/post/getpost.js
+++ pages/api/post/getpost.js
@@ -3,6 +3,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function handler(req, res){
-    const posts = await prisma.post.findMany()
+    const posts = await prisma.post.findMany({
+        select: {
+            image:true,
+            description:true,
+            user:{
+                select: {
+                    username:true,
+                }
+            }
+        }
+    })
     res.status(200).json(posts)
 }
