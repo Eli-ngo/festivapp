71d53e93fc6ba536c4e03e952620dff3c0aafcd0
Elisabeth Ngo <elisab.ngo@gmail.com>
2022-08-03 23:43:40 +0200

    profile preview image

--- pages/api/profile/[username].js
+++ pages/api/profile/[username].js
@@ -1,36 +1,36 @@
-import { PrismaClient } from "@prisma/client"
-import { getImage } from "../../../utils/formidable"
-import { uploadImage } from "../../../utils/cloudinary"
+import { PrismaClient } from "@prisma/client" // on utilise le client pour faire des requêtes à la base de données
+import { getImage } from "../../../utils/formidable" // on utilise la fonction getImage pour récupérer les images
+import { uploadImage } from "../../../utils/cloudinary" // on utilise la fonction uploadImage pour uploader les images
 
 const prisma = new PrismaClient()
 
 export const config = {api:{
-    bodyParser: false,
+    bodyParser: false, //ne transforme pas en objet les données
 }}
 
 export default async function handler (req, res) {
-    const retrieveImage = await getImage(req)
+    const retrieveImage = await getImage(req) // on récupère l'image
     console.log(retrieveImage)
-    const user = await req.query.username
-    const foundUser = await prisma.user.findUnique({
+    const user = await req.query.username // on récupère le nom d'utilisateur
+    const foundUser = await prisma.user.findUnique({ // on récupère l'utilisateur
         where: {
-            username: user,
+            username: user, // on récupère le nom d'utilisateur
         }
     })
 
-    if(foundUser){
-        if(retrieveImage){
-            const uploadedImage = await uploadImage(retrieveImage.filepath)
-            const modifyUser = await prisma.user.update({
+    if(foundUser){ // si l'utilisateur existe
+        if(retrieveImage){ // si l'image est présente
+            const uploadedImage = await uploadImage(retrieveImage.filepath) // on upload l'image sur Cloudinary
+            const modifyUser = await prisma.user.update({ // on modifie l'utilisateur
                 where:{
-                    username: foundUser.username,
+                    username: foundUser.username, // on récupère le nom d'utilisateur
                 },
                 data: {
-                    avatar: uploadedImage.url,
+                    avatar: uploadedImage.url, // on récupère l'url de l'image
                 }
             })
-            res.status(200).json({
-                username: modifyUser.username,
+            res.status(200).json({  // on renvoie un message de succès
+                username: modifyUser.username, 
                 lastname: modifyUser.lastname,
                 firstname: modifyUser.firstname,
                 email: modifyUser.email,
